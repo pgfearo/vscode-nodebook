d8147aa4f75cf3dc2e24cef0f00e779b2a3e317a
Phil Fearon <pgfearo@gmail.com>
2021-02-28 17:10:47 +0000

    use innerHTML instead of innerText

--- src/xprenderer.ts
+++ src/xprenderer.ts
@@ -2,6 +2,6 @@ const notebookApi = acquireNotebookRendererApi('xpath-html-renderer');
 
 notebookApi.onDidCreateOutput(evt => {
   const output = evt.output.data[evt.mimeType];
-  evt.element.innerText = `<pre>${output}</pre>`;
+  evt.element.innerHTML= `<pre><code style="color: green">${output}</code></pre>`;
 });
 
