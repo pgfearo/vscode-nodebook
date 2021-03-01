a39f3c45c4ef2bfb909f4013911fd2fabcb58a49
Phil Fearon <pgfearo@gmail.com>
2021-03-01 07:12:34 +0000

    wrap /path in <a> element for renderer xpath mimetype - not yet for map-entries

--- src/xprenderer.ts
+++ src/xprenderer.ts
@@ -1,7 +1,24 @@
 const notebookApi = acquireNotebookRendererApi('xpath-html-renderer');
 
 notebookApi.onDidCreateOutput(evt => {
-  const output = evt.output.data[evt.mimeType];
-  evt.element.innerHTML= `<pre><code style="color: green">${output}</code></pre>`;
+  const output: string = evt.output.data[evt.mimeType] as string;
+  const lines = output.split(/\r?\n/);
+  const outLines: string[] = [];
+  lines.forEach((line) => {
+    let tLine = line.trimLeft();
+    if (tLine.startsWith('/')) {
+      const tLineLen = tLine.length;
+      const comma = tLine.endsWith(', ')? ',' : '';
+      if (comma.length === 1) {
+        tLine = tLine.substring(0, tLineLen - 2);
+      }
+      const pad = ' '.repeat(line.length - tLineLen);
+      outLines.push(`${pad}<a>${tLine}</a><span style="color: rgba(255,255,255, 0.5)">${comma}</span>\n`)
+    } else {
+      outLines.push(line + '\n');
+    }
+  });
+  const markedLines = outLines.join('');
+  evt.element.innerHTML= `<pre><code style="text-decoration: none">${markedLines}</code></pre>`;
 });
 
