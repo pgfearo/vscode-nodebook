528a6c769fc3471def753b0a3c3bc57fc7c6c7db
Phil Fearon <pgfearo@gmail.com>
2021-04-18 09:15:49 +0100

    cell html output now uses tables for arrays of arrays arrays of objects or objects or arrays

--- src/htmlTables.ts
+++ src/htmlTables.ts
@@ -8,7 +8,7 @@ export class HtmlTables {
             if (Array.isArray(obj)) {
                 return this.constructTableForArray(obj);
             } else {
-
+                return this.constructTableForTopObject(obj);
             }
         }
     }
@@ -16,12 +16,22 @@ export class HtmlTables {
     private static constructTableForArray(array: Array<any>) {
         const tags: string[] = [];
         tags.push('<table><tbody>');
-        array.forEach((item) => {
+        // tags.push(`<tr><td><a href="command:xslt-xpath.selectXPath?${encodeURIComponent('/authors[1]/author[1]')}">/authors[1]/authors</a></td></tr>`);
+        array.forEach((item, index) => {
+            if (index === 0 && typeof item === 'object' && !Array.isArray(item)) {
+                tags.push('<thead><tr>');
+                Object.keys(item).forEach(key => tags.push(`<th scope="column">${this.escape(key)}</th>`));
+                tags.push('</tr></thead>');
+            }
             tags.push('<tr>');
             if (Array.isArray(item)) {
+                // array of arrays
                 item.forEach((innerItem) => {
                     tags.push(`<td>${this.escape(innerItem)}</td>`);
                 })
+            } else if (typeof item === 'object') {
+                // array of objects
+                Object.values(item).forEach(value => tags.push(`<td>${this.escape(value)}</td>`));
             } else {
                 tags.push(`<td>${this.escape(item)}</td>`);
             }
@@ -30,6 +40,28 @@ export class HtmlTables {
         tags.push('</tbody></table>');
         return(tags.join(''));
     }
+
+
+    private static constructTableForTopObject(obj: any) {
+        const tags: string[] = [];
+        tags.push('<table><tbody>');
+        Object.entries(obj).forEach((item) => {
+            const [key, value] = item;
+            tags.push('<tr>');
+            tags.push(`<th scope="row">${this.escape(key)}</th>`);
+            if (Array.isArray(value)) {
+                value.forEach((innerItem) => {
+                    tags.push(`<td>${this.escape(innerItem)}</td>`);
+                })
+            } else {
+                tags.push(`<td>${this.escape(value)}</td>`);
+            }
+            tags.push('</tr>');
+        });
+        tags.push('</tbody></table>');
+        return(tags.join(''));
+    }
+
 
     private static escape(obj: any) {
         if (typeof obj === 'string') {
